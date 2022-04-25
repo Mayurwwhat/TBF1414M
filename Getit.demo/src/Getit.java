6f31c9963aaf745b84c7b4a4743ead216d70fa8b
Mayurwwhat <104188995+Mayurwwhat@users.noreply.github.com>
2022-04-25 13:13:01 +0530

    Branch1
    Add m5 method

--- Getit.demo/src/Getit.java
+++ Getit.demo/src/Getit.java
@@ -10,7 +10,11 @@ public class Getit
       }	 
 	public void m4() 
       {
-    	  System.out.println("m1 call "); 
+    	  System.out.println("m4 call "); 
+      }
+		public void m5() 
+      {
+    	  System.out.println("m5 call "); 
       }
 	public static void main(String[] args) 
 	{
@@ -19,6 +23,7 @@ public class Getit
 		Getit get = new Getit();
 		get.m3();
 		get.m4();
+		get.m5();
 
 	}
 
